0c9286e01646bc8f82f08fbb47aac49bc553821a
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-04 22:36:28 -0300

    ref: fix mappings

--- mappings.lua
+++ mappings.lua
@@ -1,5 +1,17 @@
--- Neogit
-if is_available "neogit" then
-  maps.n["<leader>g"] = sections.g
-  maps.n["<leader>gg"] = { function() require("neogit").open() end, desc = "Manage Git (Neogit)" }
-end
+-- local harpoon_mark = require('harpoon.mark')
+-- local harpoon_term = require('harpoon.term')
+-- local harpoon_ui = require('harpoon.ui')
+
+return {
+  n = {
+    -- neogit
+    ["<leader>gg"] = { function() require("neogit").open() end, desc = "Manage Git (Neogit)" },
+    -- Harpoon
+    -- ['<leader>ml'] = { harpoon_ui.toggle_quick_menu },
+    -- ['<leader>mt'] = { harpoon_term.gotoTerminal },
+    -- ['<leader>mm'] = { harpoon_mark.add_file },
+    -- ['<leader>mn'] = { harpoon_ui.nav_next },
+    -- ['<leader>mb'] = { harpoon_ui.nav_prev },
+  },
+}
+
