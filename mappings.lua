0871fccf0a7935f7619b4b9721a2df1b59de6d1f
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-05 13:42:59 -0300

    enh: enable harpoon

--- mappings.lua
+++ mappings.lua
@@ -1,17 +1,17 @@
--- local harpoon_mark = require('harpoon.mark')
--- local harpoon_term = require('harpoon.term')
--- local harpoon_ui = require('harpoon.ui')
+local harpoon_mark = require('harpoon.mark')
+local harpoon_term = require('harpoon.term')
+local harpoon_ui = require('harpoon.ui')
 
 return {
   n = {
     -- neogit
     ["<leader>gg"] = { function() require("neogit").open() end, desc = "Manage Git (Neogit)" },
     -- Harpoon
-    -- ['<leader>ml'] = { harpoon_ui.toggle_quick_menu },
-    -- ['<leader>mt'] = { harpoon_term.gotoTerminal },
-    -- ['<leader>mm'] = { harpoon_mark.add_file },
-    -- ['<leader>mn'] = { harpoon_ui.nav_next },
-    -- ['<leader>mb'] = { harpoon_ui.nav_prev },
+    ['<leader>ml'] = { harpoon_ui.toggle_quick_menu },
+    ['<leader>mt'] = { harpoon_term.gotoTerminal },
+    ['<leader>mm'] = { harpoon_mark.add_file },
+    ['<leader>mn'] = { harpoon_ui.nav_next },
+    ['<leader>mb'] = { harpoon_ui.nav_prev },
   },
 }
 
