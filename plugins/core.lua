07ab323db1df2f138d4808e351a275596e11496c
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-04 22:58:45 -0300

    fix: solve missing surround setup

--- plugins/core.lua
+++ plugins/core.lua
@@ -8,7 +8,7 @@ return {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
-    config = require "user.plugins.configs.neogit"
+    config = require "user.plugins.configs.surround"
   },
   {
   "ThePrimeagen/harpoon",
